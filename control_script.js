5ff02dfbbaf2df621edf8c5f7a294d6a866997fd
Sebastian Skałacki <skalee@gmail.com>
2019-05-17 03:11:14 +0200

    Write a control script for installer

--- control_script.js
+++ control_script.js
@@ -1,3 +1,78 @@
-function Controller()
-{
+function Controller() {
+    // It tends to complain about XCode, even if all is okay.
+    installer.setMessageBoxAutomaticAnswer("XcodeError", QMessageBox.Ok);
+
+    installer.installationFinished.connect(proceed)
+}
+
+function logCurrentPage() {
+    var pageName = page().objectName
+    var pagePrettyTitle = page().title
+    console.log("At page: " + pageName + " ('" + pagePrettyTitle + "')")
+}
+
+function page() {
+    return gui.currentPageWidget()
+}
+
+function proceed(button, delay) {
+    gui.clickButton(button || buttons.NextButton, delay)
+}
+
+/// Skip welcome page
+Controller.prototype.WelcomePageCallback = function() {
+    logCurrentPage()
+    // For some reason, delay is needed.  Two seconds seems to be enough.
+    proceed(buttons.NextButton, 2000)
+}
+
+/// Just click next -- that is sign in to Qt account if credentials are
+/// remembered from previous installs, or skip sign in otherwise.
+Controller.prototype.CredentialsPageCallback = function() {
+    logCurrentPage()
+    proceed()
+}
+
+/// Skip introduction page
+Controller.prototype.IntroductionPageCallback = function() {
+    logCurrentPage()
+    proceed()
+}
+
+/// Set target directory
+Controller.prototype.TargetDirectoryPageCallback = function() {
+    logCurrentPage()
+    // TODO Use var
+    proceed()
+}
+
+Controller.prototype.ComponentSelectionPageCallback = function() {
+    logCurrentPage()
+    // TODO Select components
+    proceed()
+}
+
+/// Agree license
+Controller.prototype.LicenseAgreementPageCallback = function() {
+    logCurrentPage()
+    page().AcceptLicenseRadioButton.checked = true
+    gui.clickButton(buttons.NextButton)
+}
+
+/// Skip confirmation page
+Controller.prototype.ReadyForInstallationPageCallback = function() {
+    logCurrentPage()
+    proceed()
+}
+
+/// Installation in progress, do nothing
+Controller.prototype.PerformInstallationPageCallback = function() {
+    logCurrentPage()
+}
+
+Controller.prototype.FinishedPageCallback = function() {
+    logCurrentPage()
+    // Deselect "launch QtCreator"
+    page().RunItCheckBox.checked = false
+    proceed(buttons.FinishButton)
 }
