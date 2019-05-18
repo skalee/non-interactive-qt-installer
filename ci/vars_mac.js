f728bf3fe08ac181512ee272199b991022dad435
Sebastian Skałacki <skalee@gmail.com>
2019-05-18 12:36:54 +0200

    Test Qt LTS installation

--- ci/vars_mac.js
+++ ci/vars_mac.js
@@ -1,5 +1,6 @@
 var InstallComponents = [
-    "qt.qt5.5123.clang_64"
+    "qt.qt5.5123.clang_64",
+    "qt.qt5.598.clang_64"
 ]
 
 var InstallPath = "/opt/Qt"
--- ci/vars_win_x64.js
+++ ci/vars_win_x64.js
@@ -1,5 +1,6 @@
 var InstallComponents = [
-    "qt.qt5.5123.win64_msvc2017_64"
+    "qt.qt5.5123.win64_msvc2017_64",
+    "qt.qt5.598.win64_msvc2017_64"
 ]
 
 var InstallPath = "C:\\Qt"
